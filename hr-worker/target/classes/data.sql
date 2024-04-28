28b910216c81cf6bd3a74ade6099884c2c59dc11
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-04-27 21:22:59 -0300

    Course: HR-Microservice - git ignore copies retired

---
+++ hr-worker/target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('X', 1000.00)
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Zero', 1500.00)
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Axl', 800.00)
