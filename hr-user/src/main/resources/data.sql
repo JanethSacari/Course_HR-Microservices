b003dcab590ffe369a66571e44aaeea589565c7a
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-05-23 23:28:23 -0300

    Course: HR-Microservice - Database seeding adjust and Bcrypt

--- hr-user/src/main/resources/data.sql
+++ hr-user/src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO tb_user (name, email, password) VALUES ('X', 'x@gmail.com', '$2a$10$
 INSERT INTO tb_user (name, email, password) VALUES ('Zero', 'zero@gmail.com', '$2a$10$n//FVAoUrslGi5iOE6gCYuiZRFc6nGz1N9gwM0SorLEFl1pgvCdPu');
 
 INSERT INTO tb_role (role_name) VALUES ('ROLE_LEADER');
-INSERT INTO tb_role (role_name) VALUES ('`ROLE_RECRUIT');
+INSERT INTO tb_role (role_name) VALUES ('ROLE_HUNTER');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
