311fba3718e7b1b437b97779ff46aa06b1daba5a
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-05-24 21:49:18 -0300

    Course: HR-Microservice - User microservice with resource and repository

---
+++ hr-user/src/main/java/com/repliforce/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.repliforce.hruser.repositories;
+
+import com.repliforce.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
---
+++ hr-user/src/main/java/com/repliforce/hruser/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.repliforce.hruser.resources;
+
+import com.repliforce.hruser.entities.User;
+import com.repliforce.hruser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+}
