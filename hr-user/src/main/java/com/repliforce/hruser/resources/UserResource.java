67de74d1816e2c54ac7f041f6ae2d785505c4558
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-05-26 22:38:08 -0300

    Course: HR-Microservice - User search endpoint

--- hr-user/src/main/java/com/repliforce/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/repliforce/hruser/resources/UserResource.java
@@ -4,10 +4,7 @@ import com.repliforce.hruser.entities.User;
 import com.repliforce.hruser.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,6 +16,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findById(@RequestParam String email) {
+        User user = userRepository.findByEmail(email);
         return ResponseEntity.ok(user);
     }
 
