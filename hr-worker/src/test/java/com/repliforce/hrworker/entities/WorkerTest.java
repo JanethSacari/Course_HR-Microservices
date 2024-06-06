c8b8ee641dbb128d5adbf85c9d6a1f5e6789b961
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-05 23:04:38 -0300

    Course: HR-Microservice - Worker Test created

---
+++ hr-worker/src/test/java/com/repliforce/hrworker/entities/WorkerTest.java
@@ -0,0 +1,42 @@
+package com.repliforce.hrworker.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getDailyIncome() {
+    }
+
+    @Test
+    void setDailyIncome() {
+    }
+}
