87cf95f91203a70118a62450d0213b164cbeb86b
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-08 20:46:47 -0300

    Course: HR-Microservice - Template for Worker Resource Tests created

---
+++ hr-worker/src/test/java/com/repliforce/hrworker/repositories/WorkerRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.repliforce.hrworker.repositories;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkerRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ hr-worker/src/test/java/com/repliforce/hrworker/resources/WorkerResourceTest.java
@@ -0,0 +1,30 @@
+package com.repliforce.hrworker.resources;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkerResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getConfigs() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
