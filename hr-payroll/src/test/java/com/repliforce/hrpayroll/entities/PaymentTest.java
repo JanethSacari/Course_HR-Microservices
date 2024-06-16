613cccf3eb3759b840f5f4faece4eecb8cad4595
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-15 23:33:56 -0300

    Course: HR-Microservice - Template tests for hr-payroll

---
+++ hr-payroll/src/test/java/com/repliforce/hrpayroll/entities/PaymentTest.java
@@ -0,0 +1,46 @@
+package com.repliforce.hrpayroll.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getDailyIncome() {
+    }
+
+    @Test
+    void setDailyIncome() {
+    }
+
+    @Test
+    void getDays() {
+    }
+
+    @Test
+    void setDays() {
+    }
+
+    @Test
+    void getTotal() {
+    }
+}
---
+++ hr-payroll/src/test/java/com/repliforce/hrpayroll/resources/PaymentResourceTest.java
@@ -0,0 +1,26 @@
+package com.repliforce.hrpayroll.resources;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPayment() {
+    }
+
+    @Test
+    void getPaymentAlternative() {
+    }
+}
