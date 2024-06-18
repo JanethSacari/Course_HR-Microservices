d7c1d9b875e07ca82303f33859c171174938cbbb
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-17 21:01:29 -0300

    Course: HR-Microservice - Template tests for hr-api-gateway-zuul

---
+++ hr-api-gateway-zuul/src/test/java/com/repliforce/hrapigatewayzuul/HrApiGatewayZuulApplicationTest.java
@@ -0,0 +1,22 @@
+package com.repliforce.hrapigatewayzuul;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HrApiGatewayZuulApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
