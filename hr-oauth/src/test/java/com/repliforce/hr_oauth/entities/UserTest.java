87e1965a157ca18ca61917bb2c660933e1b9d182
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-19 23:08:41 -0300

    Course: HR-Microservice - Template tests for hr-oauth

---
+++ hr-oauth/src/test/java/com/repliforce/hr_oauth/entities/UserTest.java
@@ -0,0 +1,54 @@
+package com.repliforce.hr_oauth.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getRoles() {
+    }
+}
---
+++ hr-oauth/src/test/java/com/repliforce/hr_oauth/resources/UserResourceTest.java
@@ -0,0 +1,22 @@
+package com.repliforce.hr_oauth.resources;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+}
