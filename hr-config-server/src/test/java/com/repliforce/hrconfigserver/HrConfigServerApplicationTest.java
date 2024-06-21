053077f079e8c92c9294eaae6c27a776f8ce63aa
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-20 21:37:36 -0300

    Course: HR-Microservice - Template tests for hr-config

---
+++ hr-config-server/src/test/java/com/repliforce/hrconfigserver/HrConfigServerApplicationTest.java
@@ -0,0 +1,26 @@
+package com.repliforce.hrconfigserver;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HrConfigServerApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void run() {
+    }
+}
