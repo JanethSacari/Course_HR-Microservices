09def57169d1dd39a2700f965a018d3e2db7c61b
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2026-02-02 23:01:42 -0300

    HR Microservices - Application Test, Restoring and setting keys.

--- hr-worker/src/test/java/com/repliforce/hrworker/HrWorkerApplicationTests.java
+++ hr-worker/src/test/java/com/repliforce/hrworker/HrWorkerApplicationTests.java
@@ -1,7 +1,17 @@
 package com.repliforce.hrworker;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.NONE,
@@ -12,8 +22,51 @@ import org.springframework.boot.test.context.SpringBootTest;
 		}
 )
 class HrWorkerApplicationTests {
+	private final String[] KEYS = {
+			"eureka.client.enabled",
+			"spring.cloud.discovery.enabled",
+			"test.config"
+	};
+	private final Map<String, String> previous = new HashMap<>();
+
+	@BeforeEach
+	void setUp() {
+		for (String k : KEYS) {
+			previous.put(k, System.getProperty(k));
+		}
+		System.setProperty("eureka.client.enabled", "false");
+		System.setProperty("spring.cloud.discovery.enabled", "false");
+		System.setProperty("test.config", "default");
+	}
+
+	@AfterEach
+	void tearDown() {
+		for (String k : KEYS) {
+			String val = previous.get(k);
+			if (val == null) {
+				System.clearProperty(k);
+			} else {
+				System.setProperty(k, val);
+			}
+		}
+		previous.clear();
+	}
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mainMethodRunsWithoutThrowing() {
+		assertDoesNotThrow(() -> HrWorkerApplication.main(new String[]{}));
+	}
+
+	@Test
+	void classHasExpectedAnnotations() {
+		assertNotNull(HrWorkerApplication.class.getAnnotation(SpringBootApplication.class),
+				"SpringBootApplication annotation should be present");
+		assertNotNull(HrWorkerApplication.class.getAnnotation(EnableEurekaClient.class),
+				"EnableEurekaClient annotation should be present");
 	}
 }
--- hr-worker/src/test/java/com/repliforce/hrworker/repositories/WorkerRepositoryTest.java
+++ hr-worker/src/test/java/com/repliforce/hrworker/repositories/WorkerRepositoryTest.java
@@ -3,8 +3,6 @@ package com.repliforce.hrworker.repositories;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class WorkerRepositoryTest {
 
     @BeforeEach
