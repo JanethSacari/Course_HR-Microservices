baceeb25e60549e01d78762865565c26ffd1cccf
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2026-02-06 08:19:05 -0300

    HR Microservices - Entities tests implemented.

--- hr-user/src/test/java/com/repliforce/hruser/entities/RoleTest.java
+++ hr-user/src/test/java/com/repliforce/hruser/entities/RoleTest.java
@@ -7,36 +7,69 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoleTest {
+    private Role role;
 
     @BeforeEach
     void setUp() {
+        role = new Role(1L, "Commander");
     }
 
     @AfterEach
     void tearDown() {
+        role = null;
     }
 
     @Test
     void getId() {
+        assertEquals(1L, role.getId());
     }
 
     @Test
     void setId() {
+        role.setId(42L);
+        assertEquals(42L, role.getId());
     }
 
     @Test
     void getRoleName() {
+        assertEquals("Commander", role.getRoleName());
     }
 
     @Test
     void setRoleName() {
+        role.setRoleName("Hunter");
+        assertEquals("Hunter", role.getRoleName());
     }
 
     @Test
     void testEquals() {
+        Role a = new Role(1L, "Commander");
+        Role b = new Role(2L, "Commander");
+        Role c = new Role(3L, "Hunter");
+
+        assertTrue(a.equals(a));
+        assertTrue(a.equals(b) && b.equals(a));
+        assertFalse(a.equals(c));
+        assertFalse(a.equals(null));
+        assertFalse(a.equals(new Object()));
+
+        Role r1 = new Role(10L, null);
+        Role r2 = new Role(11L, null);
+        assertTrue(r1.equals(r2));
     }
 
     @Test
     void testHashCode() {
+        Role a = new Role(1L, "Navigator");
+        Role b = new Role(2L, "Navigator");
+        Role c = new Role(3L, "General");
+
+        assertEquals(a.hashCode(), b.hashCode());
+
+        assertNotEquals(a.hashCode(), c.hashCode());
+
+        Role r1 = new Role(10L, null);
+        Role r2 = new Role(11L, null);
+        assertEquals(r1.hashCode(), r2.hashCode());
     }
 }
--- hr-user/src/test/java/com/repliforce/hruser/entities/UserTest.java
+++ hr-user/src/test/java/com/repliforce/hruser/entities/UserTest.java
@@ -8,37 +8,65 @@ class UserTest {
 
     @Test
     void getId() {
+        User user = new User(1L, "Iris", "iris@repliforce.com", "navigatorPass");
+        assertEquals(1L, user.getId());
     }
 
     @Test
     void setId() {
+        User user = new User();
+        user.setId(42L);
+        assertEquals(42L, user.getId());
     }
 
     @Test
     void getName() {
+        User user = new User(1L, "X", "x@mavhunter.com", "hunterPass");
+        assertEquals("X", user.getName());
     }
 
     @Test
     void setName() {
+        User user = new User();
+        user.setName("Zero");
+        assertEquals("Zero", user.getName());
     }
 
     @Test
     void getEmail() {
+        User user = new User(1L, "Axl", "axl@mavhunter.com", "hunter2Pass");
+        assertEquals("axl@mavhunter.com", user.getEmail());
     }
 
     @Test
     void setEmail() {
+        User user = new User();
+        user.setEmail("colonel@repliforce.com");
+        assertEquals("colonel@repliforce.com", user.getEmail());
     }
 
     @Test
     void getPassword() {
+        User user = new User(1L, "Alia", "alia@mavhunter.com", "navigator2Pass");
+        assertEquals("navigator2Pass", user.getPassword());
     }
 
     @Test
     void setPassword() {
+        User user = new User();
+        user.setPassword("newhunterpass");
+        assertEquals("newhunterpass", user.getPassword());
     }
 
     @Test
     void getRoles() {
+        User user = new User();
+        assertNotNull(user.getRoles(), "roles set should be initialized");
+        assertTrue(user.getRoles().isEmpty(), "roles set should be empty initially");
+
+        Role role = new Role();
+        user.getRoles().add(role);
+        assertEquals(1, user.getRoles().size());
+        assertTrue(user.getRoles().contains(role));
     }
 }
