390290c26b68812c0cd60d822631121104c3677f
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2026-03-05 21:43:31 -0300

    HR Microservices - User Repository tests implemented.

--- hr-user/src/test/java/com/repliforce/hruser/repositories/UserRepositoryTest.java
+++ hr-user/src/test/java/com/repliforce/hruser/repositories/UserRepositoryTest.java
@@ -1,22 +1,40 @@
 package com.repliforce.hruser.repositories;
 
+import com.repliforce.hruser.config.AppConfig;
+import com.repliforce.hruser.entities.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@DataJpaTest
+@Import(AppConfig.class)
 class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    private User user;
 
     @BeforeEach
     void setUp() {
+        userRepository.deleteAll();
+        user = new User();
+        user.setName("Iris");
+        user.setEmail("iris@repliforce.com");
+        user = userRepository.save(user);
     }
 
     @AfterEach
     void tearDown() {
+        userRepository.deleteAll();
     }
 
     @Test
     void findByEmail() {
+        User found = userRepository.findByEmail("iris@repliforce.com");
+        assertNotNull(found, "User should be found by email");
     }
 }
