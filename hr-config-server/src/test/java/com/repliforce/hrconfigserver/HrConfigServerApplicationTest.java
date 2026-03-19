fda98da51428da62b6d3b3a245bc35889ad21e79
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2026-03-18 23:27:19 -0300

    HR-Microservices - Application tests implemented.

--- hr-config-server/src/test/java/com/repliforce/hrconfigserver/HrConfigServerApplicationTest.java
+++ hr-config-server/src/test/java/com/repliforce/hrconfigserver/HrConfigServerApplicationTest.java
@@ -1,26 +1,43 @@
 package com.repliforce.hrconfigserver;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest(properties = {
+        "spring.cloud.config.server.git.username=test-user",
+        "spring.cloud.config.server.git.uri=https://github.com/fake/repo"
+})
 class HrConfigServerApplicationTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Autowired
+    private HrConfigServerApplication application;
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    void testUsernameInjected() {
+        String username = (String) ReflectionTestUtils.getField(application, "username");
+        assertNotNull(username);
     }
 
     @Test
-    void main() {
+    void runMethodExecutesWithoutException() {
+        HrConfigServerApplication app = new HrConfigServerApplication();
+        assertDoesNotThrow(() -> app.run(),
+                "O método run deve ser executado sem lançar exceções");
     }
 
     @Test
-    void run() {
+    void mainMethodStartsApplication() {
+        System.setProperty("GH_USER", "test-user");
+        System.setProperty("GH_TOKEN", "test-token");
+
+        System.setProperty("server.port", "0");
+        System.setProperty("spring.cloud.config.server.git.uri", "https://github.com/mock/repo");
+
+        assertDoesNotThrow(() -> HrConfigServerApplication.main(new String[]{}));
     }
 }
